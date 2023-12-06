ed16aaa02034a016a5c1b8c24bf77ef67376f840
adnanbwp7 <66543071+adnanbwp7@users.noreply.github.com>
2023-12-06 15:30:47 +0500

    Fix Bugs

--- src/helper.jsx
+++ src/helper.jsx
@@ -16,10 +16,10 @@ const useFetchData = () => {
     };
 
     const fetch_price = async () => {
-        const response = await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=usd&vs_currencies=btc");
+        const response = await axios.get("https://api.coingecko.com/api/v3/simple/price?ids=usd&vs_currencies=bch");
         if (response.status == 200) {
             const data = response.data;
-            const ethPrice = data.usd.btc;
+            const ethPrice = data.usd.bch;
             const priceInBTC = ethPrice * inputValue;
             setBtcValue(priceInBTC)
         } else {
@@ -48,14 +48,15 @@ const useFetchData = () => {
         }
         const response = await axios.post(`${API_URL}/performTransaction`, body)
         console.log("🚀 ~ file: helper.jsx:51 ~ consttransaction_Approval= ~ response:", response)
-        // if (response.data.status == 200) {
-        //     const address = response.data.data.wallet_address
-        //     setWalletAddress(address)
-        //     setQrCodeUrl(`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(address)}&size=200x200`)
-        // } else {
-        //     setLoading(false)
-        //     toast.error('Error while fetcing Price!');
-        // }
+        if (response.data.status == 200) {
+            toast.success("Transaction Successfull",{toastId: "success"});
+            setInputValue('')
+            setBtcValue('')
+            setWalletAddress()
+        } else {
+            setLoading(false)
+            toast.error('Error while fetcing Price!', { toastId: "fail" });
+        }
     }
 
     const handleButtonClick = () => {
@@ -71,6 +72,9 @@ const useFetchData = () => {
                 clearTimeout(timeOutId)
             }
             timeOutId = setTimeout(() => {
+                setBtcValue()
+                setWalletAddress()
+                setQrCodeUrl('https://cdn.mainnet.cash/wait.svg')
                 fetch_price()
             }, 1000);
         }
